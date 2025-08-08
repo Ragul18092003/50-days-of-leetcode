18089f3e696498cfb41bdf3115b25b392a13fe4c
RAGUL R <169178779+Ragul18092003@users.noreply.github.com>
2025-08-08 11:45:09 +0530

    Skip to content Navigation Menu Ragul18092003 50-days-of-leetcode  Type / to search Code Issues Pull requests Actions Projects Wiki Security Insights Settings 50-days-of-leetcode/Day23 / DetectCapital.java in main  Edit  Preview Indent mode  Spaces Indent size  2 Line wrap mode  No wrap Editing DetectCapital.java file contents 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 // Day 23: Detect Capital // Problem: https://leetcode.com/problems/detect-capital/  class Solution {     public boolean detectCapitalUse(String word) {         int n=word.length();         int count=0;         for(int i=0;i<n;i++)         {             char ch=word.charAt(i);             if(ch>='A' && ch<='Z')             {                 count++;             }         }         if(count==n)         {             return true;         }         if(count==0)         {             return true;         }         if(count==1 && word.charAt(0)>='A' && word.charAt(0)<='Z')         {             return true;         }         return false;     } } Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page. New File at / · Ragul18092003/50-days-of-leetcode Commit changes Commit message Create DetectCapital.java Extended description Add an optional extended description... Direct commit or PR  Commit directly to the main branch  Create a new branch for this commit and start a pull request Learn more about pull requests

---
+++ Day23/DetectCapital.java
@@ -0,0 +1,30 @@
+// Day 23: Detect Capital
+// Problem: https://leetcode.com/problems/detect-capital/
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n=word.length();
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            char ch=word.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
+                count++;
+            }
+        }
+        if(count==n)
+        {
+            return true;
+        }
+        if(count==0)
+        {
+            return true;
+        }
+        if(count==1 && word.charAt(0)>='A' && word.charAt(0)<='Z')
+        {
+            return true;
+        }
+        return false;
+    }
+}
