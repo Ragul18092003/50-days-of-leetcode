db995511c727381fd7645c0edf6b04f70cf48123
RAGUL R <169178779+Ragul18092003@users.noreply.github.com>
2025-08-25 19:59:53 +0530

    Added Day 40: Max Consecutive Ones solution

---
+++ Day40/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+// Day 40: Max Consecutive Ones
+// Problem: https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                ++count;
+                max=Math.max(count,max);
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        return max;
+    }
+}
